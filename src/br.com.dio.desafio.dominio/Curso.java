b79adf1184f47fdaab8b586da32fed62e976380c
LawrenceUrich <leozinho.yukih@gmail.com>
2025-11-14 10:25:02 -0300

    implementação de getters, setters e toString em Mentoria e Curso

--- src/br.com.dio.desafio.dominio/Curso.java
+++
@@ -1,8 +0,0 @@
-package br.com.dio.desafio.dominio;
-
-public class Curso {
-    private String titulo;
-    private String descricao;
-    private int cargaHoraria;
-
-}
--- src/br.com.dio.desafio.dominio/Mentoria.java
+++
@@ -1,9 +0,0 @@
-package br.com.dio.desafio.dominio;
-
-import java.time.LocalDate;
-
-public class Mentoria {
-    private String titulo;
-    private String descricao;
-    private LocalDate data;
-}
